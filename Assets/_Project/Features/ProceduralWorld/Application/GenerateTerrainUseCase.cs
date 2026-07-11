12e60e02e505247287f521ad42b35b2dc2c39720
mrpromotion <142316185+mdpromotion@users.noreply.github.com>
2026-07-11 05:31:27 +0300

    Add terrain generation use case and associated meta files

---
+++ Assets/_Project/Features/ProceduralWorld/Application/GenerateTerrainUseCase.cs
@@ -0,0 +1,37 @@
+using _Project.Features.ProceduralWorld.Domain;
+using _Project.Features.ProceduralWorld.Infrastructure;
+using UnityEngine;
+
+namespace _Project.Features.ProceduralWorld.Application
+{
+    public class GenerateTerrainUseCase
+    {
+        private readonly IHeightmapGenerator _heightmapGenerator;
+        private readonly UnityTerrainWriter _terrainWriter;
+
+        public GenerateTerrainUseCase(IHeightmapGenerator heightmapGenerator, UnityTerrainWriter terrainWriter)
+        {
+            _heightmapGenerator = heightmapGenerator;
+            _terrainWriter = terrainWriter;
+        }
+
+        public void Execute(
+            TerrainData terrainData,
+            NoiseSettings settings,
+            Vector2 worldOffset)
+        {
+            float[,] heights =
+                _heightmapGenerator.Generate(
+                    terrainData.heightmapResolution,
+                    new Vector2(
+                        terrainData.size.x,
+                        terrainData.size.z),
+                    settings,
+                    worldOffset);
+
+            _terrainWriter.Apply(
+                terrainData,
+                heights);
+        }
+    }
+}
