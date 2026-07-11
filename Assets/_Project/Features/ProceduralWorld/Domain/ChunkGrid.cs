fd357381ae15090525638eb8243860bda0a12f85
mrpromotion <142316185+mdpromotion@users.noreply.github.com>
2026-07-11 23:15:52 +0300

    Add ChunkGrid class for procedural world chunk coordinate management

---
+++ Assets/_Project/Features/ProceduralWorld/Domain/ChunkGrid.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace _Project.Features.ProceduralWorld.Domain
+{
+    public class ChunkGrid
+    {
+        public float ChunkSizeX { get; }
+        public float ChunkSizeZ { get; }
+
+        public ChunkGrid(float chunkSizeX, float chunkSizeZ)
+        {
+            ChunkSizeX = chunkSizeX;
+            ChunkSizeZ = chunkSizeZ;
+        }
+
+        public Vector2 ToWorldOffset(ChunkCoordinate coordinate)
+        {
+            return new Vector2(
+                coordinate.X * ChunkSizeX,
+                coordinate.Y * ChunkSizeZ);
+        }
+
+        public ChunkCoordinate ToChunkCoordinate(Vector3 worldPosition)
+        {
+            return new ChunkCoordinate(
+                Mathf.FloorToInt(worldPosition.x / ChunkSizeX),
+                Mathf.FloorToInt(worldPosition.z / ChunkSizeZ));
+        }
+    }
+}
