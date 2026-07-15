9ca4f80f39b30c119d64f37152eba14cacd5e8b4
mrpromotion <142316185+mdpromotion@users.noreply.github.com>
2026-07-15 06:43:03 +0300

    Add Utils.SortByDistance for chunk ordering
    Introduce Utils.cs in Assets/_Project/Features/Core/Infrastructure with SortByDistance: an in-place insertion sort for List<ChunkCoordinate> that orders items by squared distance to a provided center (avoids sqrt). Also add folder .meta files for the new Infrastructure folder.

---
+++ Assets/_Project/Features/Core/Infrastructure/Utils.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using _Project.Features.ProceduralWorld.Domain;
+
+namespace _Project.Features.Core.Infrastructure
+{
+    public static class Utils
+    {
+        public static void SortByDistance(
+            List<ChunkCoordinate> list,
+            ChunkCoordinate center)
+        {
+            for (int i = 1; i < list.Count; i++)
+            {
+                ChunkCoordinate value = list[i];
+
+                int valueDistance =
+                    (value.X - center.X) * (value.X - center.X) +
+                    (value.Y - center.Y) * (value.Y - center.Y);
+
+                int j = i - 1;
+
+                while (j >= 0)
+                {
+                    ChunkCoordinate current = list[j];
+
+                    int currentDistance =
+                        (current.X - center.X) * (current.X - center.X) +
+                        (current.Y - center.Y) * (current.Y - center.Y);
+
+                    if (currentDistance <= valueDistance)
+                        break;
+
+                    list[j + 1] = current;
+                    j--;
+                }
+
+                list[j + 1] = value;
+            }
+        }
+    }
+}
